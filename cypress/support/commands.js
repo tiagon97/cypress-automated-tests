9452743dcd59aae268369759a6fbda6c5c32d6ca
tiagon <radek.wojciechowski1997@gmail.com>
2021-08-29 20:01:38 +0200

    Remove redundant commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -156,17 +156,3 @@ const handleFollowing = (method, user) => {
         },
     }).then((response) => expect(response.status).to.eq(200));
 };
-
-let LOCAL_STORAGE_MEMORY = {};
-
-Cypress.Commands.add("saveLocalStorage", () => {
-    Object.keys(localStorage).forEach((key) => {
-        LOCAL_STORAGE_MEMORY[key] = localStorage[key];
-    });
-});
-
-Cypress.Commands.add("restoreLocalStorage", () => {
-    Object.keys(LOCAL_STORAGE_MEMORY).forEach((key) => {
-        localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
-    });
-});
